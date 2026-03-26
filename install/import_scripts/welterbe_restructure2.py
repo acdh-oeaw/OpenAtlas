097dcd897c6985a6957ab28803678e6afc2f0c45
Alexander Watzinger <alexander.watzinger@craws.net>
2026-03-26 15:18:09 +0100

    Welterbe restructure 2

--- install/import_scripts/welterbe_restructure2.py
+++ install/import_scripts/welterbe_restructure2.py
@@ -1,15 +1,9 @@
 """
 This script is for restructuring type hierarchies for the Welterbe project.
 Basically some building levels types will be merged, see issue #2764
-
-# Todo
-* Move "Nutzungsart" to "Nutzungsart aktuell"
-* Make categories unselectable?
-* Delete former custom types
 """
 
 import time
-from pprint import pprint
 from typing import Any
 
 import psycopg2
@@ -17,9 +11,8 @@ from flask import g
 from psycopg2 import extras
 
 from openatlas import app
-from openatlas.models.entity import Entity, insert
-from openatlas.database import entity as db
 from openatlas.database.checks import delete_link_duplicates
+from openatlas.models.entity import Entity, insert
 
 
 def connect() -> Any:
@@ -35,15 +28,17 @@ start = time.time()
 connection = connect()
 cursor = connection.cursor(cursor_factory=extras.DictCursor)
 
+former_hierarchies = [
+    '10 Gebäude - 05 Erdgeschoß',
+    '10 Gebäude - 06 1.Obergeschoß',
+    '10 Gebäude - 06 2.Obergeschoß',
+    '10 Gebäude - 06 3.Obergeschoß',
+    '10 Gebäude - 06 4.Obergeschoß',
+    '10 Gebäude - 06 5.Obergeschoß']
+
 
 def get_mapping() -> None:
-    for name in [
-            '10 Gebäude - 05 Erdgeschoß',
-            '10 Gebäude - 06 1.Obergeschoß',
-            '10 Gebäude - 06 2.Obergeschoß',
-            '10 Gebäude - 06 3.Obergeschoß',
-            '10 Gebäude - 06 4.Obergeschoß',
-            '10 Gebäude - 06 5.Obergeschoß']:
+    for name in former_hierarchies:
         old_hierarchy = Entity.get_hierarchy(name)
         for id_ in old_hierarchy.subs:
             type_ = g.types[id_]
@@ -98,21 +93,54 @@ def insert_new_type_links() -> None:
                     'new_id': mapping[name]['subs'][sub_name]['new_id'],
                     'org_ids':
                         tuple(mapping[name]['subs'][sub_name]['org_ids'])})
+    delete_link_duplicates()
 
-def clean_up():
-    for sub_id in hierarchy.get_sub_ids_recursive():
+
+def move_usage_types() -> None:
+    usage_mapping = {}
+    current_usage_hierarchy = g.types[11329]
+    for sub_id in current_usage_hierarchy.subs:
+        usage_type = g.types[sub_id]
+        usage_mapping[usage_type.name] = sub_id
+    for name, data in mapping['Nutzungsart']['subs'].items():
+        if name not in usage_mapping:
+            new_type = insert({
+                'name': name,
+                'description': '',
+                'openatlas_class_name': 'type'})
+            new_type.link('P127', current_usage_hierarchy)
+            usage_mapping[name] = new_type.id
+        g.cursor.execute(
+            """
+            UPDATE model.link
+            SET range_id = %(new_id)s
+            WHERE property_code = 'P2' AND range_id = %(org_id)s;
+            """,
+            {'new_id': usage_mapping[name], 'org_id': data['new_id']})
+
+
+def clean_up() -> None:
+    for name in former_hierarchies:
+        hierarchy_ = Entity.get_hierarchy(name)
+        for sub_id in hierarchy_.get_sub_ids_recursive():
+            g.types[sub_id].delete()
+        hierarchy_.delete()
+    g.types = Entity.get_all_types(False)
+    former_usage_hierarchy = g.types[mapping['Nutzungsart']['new_id']]
+    for sub_id in former_usage_hierarchy.get_sub_ids_recursive():
         g.types[sub_id].delete()
+    former_usage_hierarchy.delete()
+    delete_link_duplicates()
 
 
 with app.test_request_context():
     app.preprocess_request()
     hierarchy = g.types[27145]
     hierarchy_usage = g.types[11329]
-    clean_up()
     mapping = {}
     get_mapping()
     insert_new_types()
     insert_new_type_links()
-    delete_link_duplicates()
-# pprint(mapping)
+    move_usage_types()
+    clean_up()
 print(f'Execution time: {int(time.time() - start)} seconds')
