cfb3a33d53172652a3733d40013b1ec912f745a8
Alexander Watzinger <alexander.watzinger@craws.net>
2026-06-10 13:45:06 +0200

    Merge fix from develop

--- openatlas/views/changelog.py
+++ openatlas/views/changelog.py
@@ -17,7 +17,8 @@ def index_changelog() -> str:
 versions = {
     '9.3.1': ['2026-06-12', {
         'fix': {
-            '2822': 'GeoNames import does not work'}}],
+            '2822': 'GeoNames import does not work',
+            '2821': 'Problem while entering new types/type hierarchy'}}],
     '9.3.0': ['2026-05-27', {
         'feature': {
             '2600': 'External references for references',
